d87457296071ad32309971bcbe038cb3d70fe716
Valentina Z <guaparicacoder@gmail.com>
2024-08-14 11:55:01 +0300

    Добавлена функция сохранения в формате JSON

--- main.py
+++ main.py
@@ -3,6 +3,22 @@ from tkinter import filedialog as fd
 from tkinter import messagebox as mb
 from tkinter import ttk
 import requests
+import pyperclip
+import json
+import os
+
+history_file = "upload_history.json"
+
+
+# сохраняем список загруженных файлов в формате JSON
+def save_history(file_path, link):
+    history = []
+    if os.path.exists(history_file):
+        with open(history_file, 'r') as f:
+            history = json.load(f)
+    history.append({"file_path": os.path.basename(file_path), 'download_link': link})
+    with open(history_file, 'w') as f:
+        json.dump(history, f, indent=4)
 
 
 def upload():
@@ -11,11 +27,14 @@ def upload():
         if filepath:
             with open(filepath, 'rb') as f:
                 files = {'file': f}
-                response = requests.post('https://fileio', files=files)
+                response = requests.post('https://file.io', files=files)
                 response.raise_for_status()
                 link = response.json()['link']  # получаем ссылку
                 entry.delete(0, END)  # удаляем поле с предыдущей ссылкой перед выводом новой
                 entry.insert(0, link)  # показываем ссылку в entry
+                pyperclip.copy(link)  # скопировали ссылку в буфер обмена
+                save_history(filepath, link)
+                mb.showinfo("Ссылка скопирована", f"Ссылка {link} успешно скопирована в буфер обмена")
     except Exception as e:
         mb.showerror("Ошибка", f"Произошла ошибка: {e}")
 
